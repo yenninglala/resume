8bacd59494301288f311ad26b336385b8956968b
yenninglala <47844285+yenninglala@users.noreply.github.com>
2019-05-20 05:46:40 +0800

    Add files via upload

---
+++ js/load.js
@@ -0,0 +1,26 @@
+  $(document).ready(function() {
+  $('.loader1').ClassyLoader({
+                speed: 30,
+                diameter: 80,
+                fontSize: '30px',
+                fontFamily: 'Courier',
+                fontColor: 'rgba(0,0,0,0.4)',
+                lineColor: 'rgba(255,175,255,0.7)',
+                percentage: 95,
+                lineWidth: 20,
+                start: 'top',
+                remainingLineColor: 'rgba(200,200,200,0.1)'
+            });
+   $('.loader2').ClassyLoader({
+                speed: 30,
+                diameter: 80,
+                fontSize: '30px',
+                fontFamily: 'Courier',
+                fontColor: 'rgba(0,0,0,0.4)',
+                lineColor: 'rgba(255,175,255,0.7)',
+                percentage: 95,
+                lineWidth: 20,
+                start: 'top',
+                remainingLineColor: 'rgba(200,200,200,0.1)'
+            });
+        });
